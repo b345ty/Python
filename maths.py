406847701b812a56e3f18078b281f9b09eec8d08
Charlie <b345ty@gmail.com>
2022-06-27 20:35:16 +0200

    Update maths.py

--- maths.py
+++ maths.py
@@ -5,8 +5,11 @@ print (50 + 50) # Addition
 print (50 - 50) # Subtraction
 print (50 / 50) # Division
 print (50 * 2)  # Multiply
-print (50 ** 2) # Exponents
-print (50 + 50 - 50 * 50 / 50) 
+print (50 ** 2) # Exponents 
+print (50 % 6)  # Modulo
+print (50 / 6)  # Float Number
+print (50 // 6) # Only whats left over.
+print (50 + 50 - 50 * 50 / 50) # PEMDAS
 
 # PEMDAS is P, E, MD, AS; multiplication and division have the same precedence, 
 # and the same goes for addition and subtraction. 
