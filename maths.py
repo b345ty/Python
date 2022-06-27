c961664e7073a9e92b5b3009a56dec9a6af5ba0e
Charlie <b345ty@gmail.com>
2022-06-27 20:18:44 +0200

    Create maths.py

---
+++ maths.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+# Python Maths
+
+print (50 + 50) # Addition
+print (50 - 50) # Subtraction
+print (50 / 50) # Division
+print (50 * 2)  # Multiply
+print (50 ** 2) # Power Of
+print (50 + 50 - 50 * 50 / 50) 
+
+# PEMDAS is P, E, MD, AS; multiplication and division have the same precedence, 
+# and the same goes for addition and subtraction. 
+# When a division operator appears before multiplication, division goes first.
